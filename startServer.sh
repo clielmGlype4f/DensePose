2f17d1820c8bde5df6d299c361ddf9402792e6a5
Cristobal Valenzuela <4291744+clielmGlype4f@users.noreply.github.com>
2018-07-24 18:35:38 -0400

    set server script

--- startServer.sh
+++ startServer.sh
@@ -1,4 +1,5 @@
 nvidia-docker run --rm -it -v /home/paperspace/DensePose/DensePoseData:/denseposedata \
-  -v /home/paperspace/Datasets/coco:/coco \
+  -v /home/paperspace/Datasets/coco:/coco \ 
+  -v /home/paperspace/DensePose/server/:/densepose/server \
   -p 22100:22100 \
-  denseposeserver:latest
+  --entrypoint "bash" denseposeserver:latest
